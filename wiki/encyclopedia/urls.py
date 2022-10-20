0ed15ab53236fbce16cf04bd3891e1d1792b8c4e
= <=>
2022-10-20 10:40:47 +0400

    add_page done

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add_page/", views.add_page, name="add_page"),
+    path("random_page/", views.random_page, name="random_page"),
+    path("<str:title>/", views.get_page, name="get_page"),
+
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,14 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
 from . import util
+
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    page_text = forms.CharField(label="Page text")
 
 
 def index(request):
@@ -8,3 +16,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def add_page(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = "# " + title + "\n" + form.cleaned_data["page_text"]
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+        else:
+            return render(request, "encyclopedia/add_page.html", {
+                "form": NewPageForm()})
+    return render(request, "encyclopedia/add_page.html", {
+        "form": NewPageForm()
+    })
+
+
+def random_page(request):
+    return render(request, "encyclopedia/random_page.html")
+
+
+def get_page(request):
+    return render(request, "encyclopedia/get_page.html",)
